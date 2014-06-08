c7f72bd22a193319203a224057a6c780217f3a8a
Junyi Xie <junyi.xie@yahoo.com>
2014-06-07 22:15:11 -0400

    add cscope support for java

--- emacs/emacs-junyi.el
+++ emacs/emacs-junyi.el
@@ -1,7 +1,47 @@
 ;;; add search path
-(add-to-list 'load-path "/Users/junyi/dotfiles/emacs")
-(add-to-list 'load-path "/usr/bin")
-(add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.6.4/emacs")
+(add-to-list 'load-path "/Users/junyi/.emacs.d/cc-mode/")
+(add-to-list 'load-path "/Users/junyi/.emacs.d/")
+
+;;; display date and time
+(display-time-mode 1)
+(setq display-time-format "%R %m-%d-%Y")
+(setq display-time-day-and-date t) ;;
+(setq display-time-24hr-format t)
+(display-time)
+
+(require 'cc-mode)
+(c-initialize-cc-mode t)
+;;;(c-init-language-vars-for 'java-mode)
+;;;(c-common-init 'java-mode) ; Or perhaps (c-basic-common-init 'some-mode)
+
+;;; show whitespaces
+(setq-default show-trailing-whitespace t)
+
+;;; tab width
+(setq default-tab-width 4)
+(setq indent-tabs-mode nil)
+
+;;; java mode indent
+(require 'java-mode-indent-annotations)
+
+;;; smart scroll
+(require 'smooth-scroll)
+(smooth-scroll-mode t)
+
+;;; long lines mode
+(autoload 'longlines-mode
+  "longlines.el"
+  "Minor mode for automatically wrapping long lines." t)
+
+;;; workgroups to restore windows
+(require 'workgroups)
+(workgroups-mode 1)
+(setq wg-switch-on-load nil)
+(wg-load "/Users/junyi/.emacs.d/default.layout")
+
+;;; cscope support ;;;
+(require 'xcscope)
+(cscope-setup)
 
 ;;; elrang support
 (setq erlang-root-dir "/usr/local/lib/erlang")
@@ -10,32 +50,22 @@
 (add-hook 'erlang-mode-hook
 		  #'(lambda () (setq indent-tabs-mode nil)))
 
-;;; show whitespaces
-(setq-default show-trailing-whitespace t)
 
-;;; tab width
-(setq default-tab-width 4)
-
-;;; smart scroll
-(require 'smooth-scroll)
-(smooth-scroll-mode t)
-
-;;; long lines mode
-(autoload 'longlines-mode
-          "longlines.el"
-           "Minor mode for automatically wrapping long lines." t)
-
-;;; workgroups to restore windows
-(require 'workgroups)
-(workgroups-mode 1)
-(setq wg-switch-on-load 1)
-(wg-load "/Users/junyi/dotfiles/emacs/default.layout")
+;;; try to display the buffer in an existing window
+;;; without creating new windows on the selected frame.
+(setq split-height-threshold nil)
+(setq split-width-threshold nil)
 
 ;; function key map
-(define-key function-key-map "\e\eOA" [(meta up)])
-(define-key function-key-map "\e\eOB" [(meta down)])
-(define-key function-key-map "\e\eOC" [(meta right)])
-(define-key function-key-map "\e\eOD" [(meta left)])
+(define-key input-decode-map "\e\eOA" [(meta up)])
+(define-key input-decode-map "\e\eOB" [(meta down)])
+(global-set-key [(meta down)]    'scroll-up-1)
+(global-set-key [(meta up)]      'scroll-down-1)
+
+;;;(define-key function-key-map "\e\eOA" [(meta up)])
+;;;(define-key function-key-map "\e\eOB" [(meta down)])
+;;;(define-key function-key-map "\e\eOC" [(meta right)])
+;;;(define-key function-key-map "\e\eOD" [(meta left)])
 ;;;(define-key function-key-map "\eOa" [(control up)])
 ;;;(define-key function-key-map "\eOb" [(control down)])
 ;;;(define-key function-key-map "\eOc" [(control right)])
@@ -43,9 +73,6 @@
 
 ;; (define-key function-key-map "\e[T" [(shift tab)])
 ;; (define-key function-key-map "\e[U" [(control tab)])
-
-(global-set-key [(meta down)]    'scroll-up-1)
-(global-set-key [(meta up)]      'scroll-down-1)
 
 ;;; Emulation of the vi % command
 (defun goto-match-paren (arg)
@@ -60,9 +87,9 @@ vi style of % jumping to matching brace."
 ;;; Line to top of window;
 ;;; replace three keystroke sequence  C-u 0 C-l
 (defun line-to-top-of-window ()
-       "Move the line point is on to top of window."
-       (interactive)
-       (recenter 0))
+  "Move the line point is on to top of window."
+  (interactive)
+  (recenter 0))
 (global-set-key [f6] 'line-to-top-of-window)
 
 ;;; remap previous input [M-p]
@@ -86,40 +113,23 @@ vi style of % jumping to matching brace."
 ;;; show line and col number
 (setq line-number-mode t)
 (setq column-number-mode t)
-
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
-
 
 ;; resize windows
 (global-set-key [f7]      'shrink-window-horizontally)
 (global-set-key [f8]      'enlarge-window-horizontally)
 (global-set-key [f9]      'shrink-window)
-(global-set-key [f10]      'enlarge-window)
-
-;;; -------------- ;;;
-;;; cscope support ;;;
-;;; -------------- ;;;
-
-;;; (require 'xcscope)
-
-;;	(define-key global-map [(control f3)]  'cscope-set-initial-directory)
-;;	(define-key global-map [(control f4)]  'cscope-unset-initial-directory)
-;;	(define-key global-map [(control f5)]  'cscope-find-this-symbol)
-
-;;	(define-key global-map [(control f7)]
-;;	  'cscope-find-global-definition-no-prompting)
-;;	(define-key global-map [(control f8)]  'cscope-pop-mark)
-;;	(define-key global-map [(control f9)]  'cscope-next-symbol)
-;;	(define-key global-map [(control f10)] 'cscope-next-file)
-;;	(define-key global-map [(control f11)] 'cscope-prev-symbol)
-;;	(define-key global-map [(control f12)] 'cscope-prev-file)
-;;      (define-key global-map [(meta f9)]  'cscope-display-buffer)
-;;      (defin-ekey global-map [(meta f10)] 'cscope-display-buffer-toggle)
+(global-set-key [f10]     'enlarge-window)
 
 ;;; cscope keybinding
-(define-key global-map [(meta ..)] 'cscope-find-global-definition)
-(define-key global-map [(meta .r)] 'cscope-find-functions-calling-this-function)
+(global-set-key (kbd "C-c s") 'cscope-find-this-symbol)
+(global-set-key (kbd "C-c d") 'cscope-find-global-definition)
+(global-set-key (kbd "C-c r") 'cscope-find-functions-calling-this-function)
+(global-set-key (kbd "C-c c") 'cscope-find-called-functions)
+(global-set-key (kbd "C-c f") 'cscope-find-this-file)
+(global-set-key (kbd "C-c g") 'cscope-find-egrep-pattern)
+(global-set-key (kbd "C-c =") 'cscope-find-assignments-to-this-symbol)
+(global-set-key (kbd "C-c #") 'cscope-find-files-including-file)
 
-;(global-set-key (kbd "M-.") 'cscope-find-global-definition)
-;(global-set-key (kbd "\M-.r") 'cscope-find-functions-calling-this-function)
+
